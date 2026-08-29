82ddd4cbd3990070c65eb309a8bec7c3f0e40b26
Will <barbosa.will.1998@gmail.com>
2026-08-29 14:33:12 -0300

    chore: remove the stale countries pipeline, duplicate types and orphan util

--- lib/utils/getCountryNameInLanguage.ts
+++
@@ -1,9 +0,0 @@
-import { ICountrySelectLanguages, ICountry } from '../interface';
-import { getCountryName } from './getCountryName';
-
-export const getCountryNameInLanguage = (
-  country: ICountry,
-  language: ICountrySelectLanguages = 'eng'
-): string => {
-  return getCountryName(country, language);
-};
